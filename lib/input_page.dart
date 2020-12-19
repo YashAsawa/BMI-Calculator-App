e43f4219d116a0b0fb83a83dfa471b1d34ff4f12
YashAsawa <yash17bce2296@gmail.com>
2020-12-19 23:49:27 +0530

    Implemented the navigation to the second page

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -3,6 +3,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'icon_content.dart';
 import 'reusable_card.dart';
 import 'package:bmi_calculator/constants.dart';
+import 'package:bmi_calculator/results_page.dart';
 
 enum Gender {
   male,
@@ -205,11 +206,18 @@ class _InputPageState extends State<InputPage> {
               ),
                ],
                 ),),
-          Container(
-            color: kBottomContainerColour,
-            margin: EdgeInsets.only(top: 10.0),
-            width: double.infinity,
-            height: kBottomContainerHeight,
+          GestureDetector(
+            onTap:() {
+              Navigator.push(context,
+                 MaterialPageRoute(builder: (context) => ResultsPage()) );
+            },
+            child: Container(
+              child: Text('Calculate'),
+              color: kBottomContainerColour,
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: kBottomContainerHeight,
+            ),
           ),
         ],
       ),
