6cea4b0b13710aee8f3d30dc57bedfa87d2ad9fe
YashAsawa <yash17bce2296@gmail.com>
2020-12-20 13:45:12 +0530

    Completed the Results Page. Created two new directories to categorise files. Moved the bottom button and round icon button to separate files.

--- lib/Screens/input_page.dart
+++ lib/Screens/input_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'icon_content.dart';
-import 'reusable_card.dart';
+import 'package:bmi_calculator/Components/icon_content.dart';
+import 'package:bmi_calculator/Components/reusable_card.dart';
 import 'package:bmi_calculator/constants.dart';
-import 'package:bmi_calculator/results_page.dart';
+import 'package:bmi_calculator/Screens/results_page.dart';
+import 'package:bmi_calculator/Components/bottom_button.dart';
+import 'package:bmi_calculator/Components/round_icon_button.dart';
 
 enum Gender {
   male,
@@ -207,23 +209,12 @@ class _InputPageState extends State<InputPage> {
               ],
             ),
           ),
-          GestureDetector(
+          BottomButton(
+            buttonTitle: 'CALCULATE',
             onTap: () {
               Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => ResultsPage()));
+                  MaterialPageRoute(builder: (context) => ResultsPage(),),);
             },
-            child: Container(
-              child: Center(
-                child: Text(
-                  'CALCULATE',
-                  style: kLargeButtonTextStyle,
-                ),
-              ),
-              color: kBottomContainerColour,
-              margin: EdgeInsets.only(top: 10.0),
-              width: double.infinity,
-              height: kBottomContainerHeight,
-            ),
           ),
         ],
       ),
@@ -231,23 +222,5 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
-class RoundIconButton extends StatelessWidget {
-  RoundIconButton({@required this.icon, @required this.onPressed});
-  final IconData icon;
-  final Function onPressed;
 
-  @override
-  Widget build(BuildContext context) {
-    return RawMaterialButton(
-      child: Icon(icon),
-      onPressed: onPressed,
-      elevation: 0.0,
-      constraints: BoxConstraints.tightFor(
-        width: 56.0,
-        height: 56.0,
-      ),
-      shape: CircleBorder(),
-      fillColor: Color(0xFF4C4F5E),
-    );
-  }
-}
+
---
+++ lib/Screens/results_page.dart
@@ -0,0 +1,63 @@
+import 'package:bmi_calculator/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:bmi_calculator/Components/reusable_card.dart';
+import 'package:bmi_calculator/Components/bottom_button.dart';
+
+class ResultsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(15.0),
+              alignment: Alignment.center,
+              child: Text(
+                  'Your Result',
+              style: kTitleTextStyle,
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          Expanded(
+              flex: 5,
+            child: ReusableCard(
+              colour: kActiveCardColour,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[Text(
+                'Normal',
+                  style: kResultTextStyle ,
+                ),
+                Text(
+                  '18.3',
+                  style: kBmiTextStyle,
+                ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(
+                      'Your BMI result is quite low\nYou should eat more',
+                      style: kBodyTextStyle,
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            ) ,
+          ),
+          BottomButton(buttonTitle: 'RE-CALCULATE',
+            onTap:() {
+            Navigator.pop(context);
+          }, ),
+        ],
+      ),
+    );
+  }
+}
